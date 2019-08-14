8146ab185e407bac49389d9da98d396c5d34b42b
Kwoth <master.kwoth@gmail.com>
2019-08-15 01:59:01 +0200

    Temporary bugfix for patreon rewards

--- NadekoBot.Core/Services/Database/Models/RewardedUser.cs
+++ NadekoBot.Core/Services/Database/Models/RewardedUser.cs
@@ -4,6 +4,10 @@ namespace NadekoBot.Core.Services.Database.Models
 {
     public class RewardedUser : DbEntity
     {
+        /// <summary>
+        /// OBSOLETE, DO NOT USE
+        /// </summary>
+        public ulong UserId { get; set; } = 0;
         public string PatreonUserId { get; set; }
         public int AmountRewardedThisMonth { get; set; }
         public DateTime LastReward { get; set; }
