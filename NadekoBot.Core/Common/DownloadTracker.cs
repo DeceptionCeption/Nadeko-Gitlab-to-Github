beb7014df0269578225d0600fd2760ab38d1869f
Kwoth <master.kwoth@gmail.com>
2020-09-16 16:19:47 +0200

    disable ensureusersdownloaded on public bot due to memory usage

--- NadekoBot.Core/Common/DownloadTracker.cs
+++ NadekoBot.Core/Common/DownloadTracker.cs
@@ -18,27 +18,28 @@ namespace NadekoBot.Core.Common
         /// </summary>
         /// <param name="guild">Guild to check and potentially download users from</param>
         /// <returns>Task representing download state</returns>
-        public async Task EnsureUsersDownloadedAsync(IGuild guild)
+        public Task EnsureUsersDownloadedAsync(IGuild guild)
         {
-            await downloadUsersSemaphore.WaitAsync();
-            try
-            {
-                var now = DateTime.UtcNow;
-
-                // download once per hour at most
-                var added = LastDownloads.AddOrUpdate(
-                    guild.Id,
-                    now,
-                    (key, old) => (now - old) > TimeSpan.FromHours(1) ? now : old);
-
-                // means that this entry was just added - download the users
-                if (added == now)
-                    await guild.DownloadUsersAsync();
-            }
-            finally
-            {
-                downloadUsersSemaphore.Release();
-            }
+            return Task.CompletedTask;
+            // await downloadUsersSemaphore.WaitAsync();
+            // try
+            // {
+            //     var now = DateTime.UtcNow;
+            //
+            //     // download once per hour at most
+            //     var added = LastDownloads.AddOrUpdate(
+            //         guild.Id,
+            //         now,
+            //         (key, old) => (now - old) > TimeSpan.FromHours(1) ? now : old);
+            //
+            //     // means that this entry was just added - download the users
+            //     if (added == now)
+            //         await guild.DownloadUsersAsync();
+            // }
+            // finally
+            // {
+            //     downloadUsersSemaphore.Release();
+            // }
         }
     }
 }
